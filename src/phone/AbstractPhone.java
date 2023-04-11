6ef603edff830096fddd383c951cabb0b6aedf01
KirillMar <re4r@bk.ru>
2023-04-11 19:09:17 +0300

    phone

--- src/phone/AbstractPhone.java
+++ src/phone/AbstractPhone.java
@@ -1,4 +1,13 @@
 package phone;
 
 public abstract class AbstractPhone {
+    private int year;
+
+    public AbstractPhone(int year) {
+        this.year = year;
+    }
+
+    public abstract void call(int outputNumber);
+
+    public abstract void ring(int inputNumber);
 }
