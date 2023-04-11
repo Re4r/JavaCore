7545f12e24d6111db9653b790e4c55947656b471
KirillMar <re4r@bk.ru>
2023-04-11 20:04:28 +0300

    phone
    Signed-off-by: KirillMar <re4r@bk.ru>

--- src/phone/User.java
+++ src/phone/User.java
@@ -1,6 +1,19 @@
 package phone;
 
 public class User {
+
+    public static void main(String[] args) {
+        AbstractPhone cellPhone = new CellPhone(2003, 24);
+        AbstractPhone smartPhone = new SmartPhone(2015, 36, "Android");
+
+        User user = new User("Derta");
+
+        user.callAnotherUser(234567, cellPhone);
+        user.callAnotherUser(678905, smartPhone);
+        user.incomingCall(123456,cellPhone);
+        user.incomingCall(14564,smartPhone);
+    }
+
     private String name;
 
     public User(String name) {
@@ -9,5 +22,9 @@ public class User {
 
     public void callAnotherUser(int number, AbstractPhone phone) {
         phone.call(number);
+    }
+
+    public void incomingCall(int number, AbstractPhone phone) {
+        phone.ring(number);
     }
 }
