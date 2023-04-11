ea9eb3afb606bcf523f8fa0c81c285a27788c4b0
KirillMar <re4r@bk.ru>
2023-04-11 19:47:25 +0300

    phone
    Signed-off-by: KirillMar <re4r@bk.ru>

---
+++ src/phone/SmartPhone.java
@@ -0,0 +1,15 @@
+package phone;
+
+public class SmartPhone extends CellPhone {
+
+    private String operationSystem;
+
+    public SmartPhone(int year, int workTime, String operationSystem) {
+        super(year, workTime);
+        this.operationSystem = operationSystem;
+    }
+
+    public void install(String program) {
+        System.out.println("Install " + program + " for " + operationSystem);
+    }
+}
