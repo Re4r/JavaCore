13baa04ef32f043181865cb0cc2d70c8e2ef9c81
KirillMar <re4r@bk.ru>
2023-04-11 19:32:46 +0300

    phone

---
+++ src/phone/SomePhone.java
@@ -0,0 +1,29 @@
+package phone;
+
+public class SomePhone extends AbstractPhone {
+
+    private String company;
+
+    public SomePhone(int year, String company) {
+        super(year);
+        this.company = company;
+    }
+
+    private void openConnection() {
+        System.out.println("Find commutator...");
+        System.out.println("Open new connection...");
+    }
+
+
+    @Override
+    public void call(int outputNumber) {
+        openConnection();
+        System.out.println("Calling to number...");
+    }
+
+    @Override
+    public void ring(int inputNumber) {
+        System.out.println("Incoming call...");
+
+    }
+}
