e9607fbd4def2ee80088711283e6add20734d392
KirillMar <re4r@bk.ru>
2023-04-17 15:57:37 +0300

    solution Signed-off-by: KirillMar <re4r@bk.ru>
    Signed-off-by: KirillMar <re4r@bk.ru>

---
+++ src/level06/Solution3.java
@@ -0,0 +1,24 @@
+package level06;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution3 {
+    public static volatile List<Thread> list = new ArrayList<Thread>(5);
+
+    public static void main(String[] args) {
+
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+
+    }
+
+    public static class SpecialThread implements Runnable {
+        public void run() {
+            System.out.println("it's a run method inside SpecialThread");
+        }
+    }
+}
